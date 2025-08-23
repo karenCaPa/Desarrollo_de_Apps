175ca64e59a6fef4181afd515cdd81c4eeda2c43
karenCaPa <karen_camachopa@fet.edu.co>
2025-08-22 22:47:50 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,11 +6,27 @@
 const input = document.getElementById('input');
 const agregarBtn = document.getElementById('agregarBtn');
 const lista = document.getElementById('lista');
+
 agregarBtn.addEventListener('click', function() {
-const nuevaTareaTexto = input.value;
-if (nuevaTareaTexto === '') return;
-const nuevaTareaLi = document.createElement('li');
-nuevaTareaLi.textContent = nuevaTareaTexto;
-lista.appendChild(nuevaTareaLi);
-input.value = '';
+    const nuevaTareaTexto = input.value.trim();
+    if (nuevaTareaTexto === '') return;
+
+    
+    const nuevaTareaLi = document.createElement('li');
+    nuevaTareaLi.textContent = nuevaTareaTexto;
+
+    const btnEliminar = document.createElement('button');
+    btnEliminar.textContent = 'Eliminar';
+    btnEliminar.style.marginLeft = '10px';
+
+    
+    btnEliminar.addEventListener('click', function() {
+        nuevaTareaLi.remove();
+    });
+
+    
+    nuevaTareaLi.appendChild(btnEliminar);   
+    lista.appendChild(nuevaTareaLi);
+
+    input.value = '';
 });
